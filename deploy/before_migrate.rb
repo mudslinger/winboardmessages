e7ab02fe610fc58b22c0dbbce286ac749224a0d6
hisato tanaka <hisato.tanaka@gmail.com>
2013-10-16 20:44:50 +0900

    6

--- deploy/before_migrate.rb
+++
@@ -1,9 +0,0 @@
-Chef::Log.info("Running deploy/before_migrate.rb")
-env = node[:deploy][:winboardmessages][:rails_env]
-current_release = release_path
-
-execute "sidekiq" do
-  cwd current_release
-  command "bundle exec sidekiq --daemon --logfile log/sidekiq.log"
-  environment "RAILS_ENV" => env
-end
