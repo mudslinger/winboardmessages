9377478be95551c7b627692f0d3dce4390718122
hisato tanaka <hisato.tanaka@gmail.com>
2013-10-24 18:01:33 +0900

    12

--- app/controllers/top_message_controller.rb
+++ app/controllers/top_message_controller.rb
@@ -8,12 +8,21 @@ class TopMessageController < ApplicationController
     @type = params[:type] if present?
     @key = 0
     @key = params[:key].to_i if present?
-    list = Redis.current.lrange(@type,(@key == 0 ? 0 : @key-1),@key+1).map{ |item|
-      OpenStruct.new(MessagePack.unpack(item)).extend(Message)
-    }
-    @entry = @key == 0 ? list[0] : list[1]
-    @prev_entry = @key == 0 ? nil : list[0]
-    @next_entry = @key == 0 ? list[1] : list[2]
+
+    list = nil
+    cache_key = [@type,@key].join('#')
+    begin
+      list = Redis.current.lrange(@type,(@key == 0 ? 0 : @key-1),@key+1).map{ |item|
+        OpenStruct.new(MessagePack.unpack(item)).extend(Message)
+      }
+      Rails.cache.write(cache_key,list,:expires_in => 10.minutes) unless Rails.cache.read(cache_key)
+    rescue
+      list = Rails.cache.read(cache_key)
+    ensure
+      @entry = @key == 0 ? list[0] : list[1]
+      @prev_entry = @key == 0 ? nil : list[0]
+      @next_entry = @key == 0 ? list[1] : list[2]
+    end
   end
 
   private
