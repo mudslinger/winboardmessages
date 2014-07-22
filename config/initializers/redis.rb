00d242f1a19d8eeb59ae084a3b0c77b5fa0c7625
hisato tanaka <hisato.tanaka@gmail.com>
2014-07-22 11:53:31 +0900

    vps change

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,7 +1,7 @@
 Redis.current = Redis::Namespace.new(
   "yamaokaya:wbmessages",
   redis: Redis.new(
-    host: 'yamapp-redis.gneihq.0001.apne1.cache.amazonaws.com',
+    host: 'yam-redis.gneihq.0001.apne1.cache.amazonaws.com',
     port: 6379
   )
 )
